dbab66e851d7b4e1c7c6e98f2a287c86a005657a
Vitória Freitas <vitoriafreitas110903@gmail.com>
2025-03-26 13:48:13 -0300

    Atualizado

--- projetos, exercicios/js/minhas atividades/exercicios de treino/js3.js
+++ projetos, exercicios/js/minhas atividades/exercicios de treino/js3.js
@@ -1,6 +1,6 @@
 // exercicio hash table
 
-class hashTable{
+class HashTable{
     constructor(size){
         this.table = new Array(size)
         this.size = size
@@ -8,8 +8,8 @@ class hashTable{
 
     hash(nome){
         let total = 0           
-        for(let i = 0; i < nome.lenght; i++){
-            total = total + nome.charCodeAt(i)
+        for(let i = 0; i < nome.length; i++){
+            total += nome.charCodeAt(i)
         }
         return total % this.size
     }
@@ -17,5 +17,53 @@ class hashTable{
     adicionar(nome, telefone){
         const index = this.hash(nome)
         const bucket = this.table[index]
+        if(!bucket){
+            this.table[index] = [[nome, telefone]]
+        } else{
+            const mesmoNome = bucket.find(item => item[0] === nome)
+            if(mesmoNome){
+                mesmoNome[1] = nome
+            } else {
+                bucket.push([nome, telefone])
+            }
+        }
+    }
+
+    buscar(nome){
+        const index = this.hash(nome)
+        const bucket = this.table[index]
+        if(bucket){
+            const mesmoNome = bucket.find(item => item[0] === nome)
+            if(mesmoNome){
+                return mesmoNome
+            }
+        } 
+        return undefined
+    }
+
+    remover(nome){
+        const index = this.hash(nome)
+        const bucket = this.table[index]
+        if(bucket){
+            const mesmoNome = bucket.find(item => item[0] === nome)
+            if(mesmoNome){
+                bucket.splice(bucket.indexOf(mesmoNome), 1)
+            }
+        }
+    }
+
+    mostrar(){
+        for(let i = 1; i < this.table.length; i++){
+            if(this.table[i]){
+                console.log(i, this.table[i])
+            }
+        }
     }
 }
+
+const lista = new HashTable(50)
+
+lista.adicionar("João", 11985554575)
+lista.adicionar("Felipe", 11982544961)
+lista.mostrar()
+console.log(lista.buscar("João"))
---
+++ projetos, exercicios/js/minhas atividades/pizzaria/js/script.js
@@ -0,0 +1,31 @@
+// botões de adicionar as pizzas
+const adiPrecoBtnCal = document.querySelector("#adicionarPrecoCal")
+const adiPrecoBtnPor = document.querySelector("#adicionarPrecoPor")
+const adiPrecoBtnFran = document.querySelector("#adicionarPrecoFran")
+const adiPrecoBtnPep = document.querySelector("#adicionarPrecoPep")
+// preços das pizzas
+const calPreco = document.querySelector("#calpreco")
+const porPreco = document.querySelector("#porpreco")
+const franPreco = document.querySelector("#franpreco")
+const pepPreco = document.querySelector("#peppreco")
+// divs
+const divCarrinho = document.querySelector("#carrinho")
+const divFinalizar = document.querySelector("#finalizar")
+// botões SIM e NAO
+const btnSim = document.querySelector("#botaosim")
+const btnNao = document.querySelector("#botaonao")
+// preço total
+const precoTotal = document.querySelector("#precoTotal")
+// botão de finalizar
+const finalizarCompraBtn = document.querySelector("#btnFinalizarCompra")
+
+// funções
+
+
+
+
+// eventos
+
+adiPrecoBtnCal.addEventListener("click", () => {
+
+})
