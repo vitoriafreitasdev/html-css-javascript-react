6d63582e278a863477dd7329d5a61534f67f7471
Vitória Freitas <vitoriafreitas110903@gmail.com>
2025-05-02 15:16:08 -0300

    ok

--- projetos, exercicios/react/css_react/css_react/src/App.jsx
+++ projetos, exercicios/react/css_react/css_react/src/App.jsx
@@ -2,8 +2,15 @@ import './App.css'
 
 // css  de componente
 import MyComponents from './components/MyComponents' 
+import Title from './components/Title'
 
 function App() {
+  // CSS inline dinamico
+  const n = 15
+
+  // classes dinamicas
+  const redTitle = true
+
   return (
     <div className='App'>
       {/* css global */}
@@ -11,6 +18,21 @@ function App() {
       {/* css de componente */}
       <MyComponents/>
       <p>Pegou o CSS do componente</p>
+      {/* inline style */}
+      <p style={{ color: "blue", padding:"25px", borderTop: "1px dotted blue"}}>Este elemento tem estilos inline</p>
+      {/* inline style dinamico */}
+      <h2 style={n > 10 ? {color: "purple"} : { color: "magenta"}}>
+        CSS dinâmico
+      </h2>
+      <h2 style={n > 20 ? { color: "purple"} : {color: "magenta"}}>
+        CSS dinâmico
+      </h2>
+      {/* classes dinamicas */}
+      <h1 className={redTitle ? "red-title" : "title"}>
+        Este título vai ter uma classe
+      </h1>
+      {/* css module */}
+      <Title/>
     </div>
   )
 }
---
+++ projetos, exercicios/react/css_react/css_react/src/components/Title.jsx
@@ -0,0 +1,9 @@
+import classes from "./Title.module.css"
+
+const Title = () => {
+  return (
+    <h1 className={classes.title}>Meu título!</h1>
+  )
+}
+
+export default Title
