1582e09f6367ce2e4e39233f03a2de2160f33d03
Vitória Freitas <vitoriafreitas110903@gmail.com>
2025-07-17 11:01:17 -0300

    Ok

--- projetosEexercicios/reactPartyTime/frontend/src/hook/useToast.jsx
+++ projetosEexercicios/reactPartyTime/frontend/src/hook/useToast.jsx
@@ -3,9 +3,21 @@ import { toast } from "react-toastify";
 const useToast = (msg, status = null) => {
 
     if(!status) {
-        toast.success(msg, {})
+        toast.success(msg, {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            theme: "light"
+        })
     } else if(status === "error") {
-        toast.error(msg, {})
+        toast.error(msg, {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            theme: "light"
+        })
     }
 }
 
